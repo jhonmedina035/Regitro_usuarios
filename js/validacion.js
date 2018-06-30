c5c19343b75fe16734c51ddf2c4f5e89f7f9c55f
jhon <jhonmedina035@gmail.com>
2018-06-29 20:40:42 -0600

    mejorando el interfaz de usario

--- js/validacion.js
+++ js/validacion.js
@@ -1,40 +1,83 @@
 
+function validarNumeros(valoHtml,itm){
+    var expresion = /^[0-9]+$/;
+    if (expresion.test(valoHtml.value)){
+       valoHtml.style.borderColor="#80FF00";
+       valor = true;
+    }
+    else{
+        valoHtml.style.borderColor="#FF0000";
+        valor = false;
+    }
+}
+function validarAlfabetico(valoHtml,itm){
+    var expresion = /^([ñ a-z A-Z ñáéíóú]{2,60})$/;
+    if(expresion.test(valoHtml.value)){
+        valoHtml.style.borderColor="#80FF00";
+        valor = true;
+    }
+    else{
+        valoHtml.style.borderColor="#FF0000";
+        valor = false;
+          
+    }
+}
+function validarSubmit(){  
+    var profesion =document.getElementById("txtProfesion");
+    var identificacion = document.getElementById("txtIdentificacion");
+    var nombre = document.getElementById("txtNombre");
+    var apellidos = document.getElementById("txtApellidos");
+    var edad = document.getElementById("txtEdad");
+    var itm = 0;
+    if(identificacion.value ===""|| nombre.value===""|| apellidos.value===""|| edad.value===""|| profesion.value ==="" ){ 
+        alert ("Todos los campos deven estar diligenciados para continuar")
+        return false;
+    }
+    valoHtml= identificacion
+    validarNumeros(valoHtml,itm);
+   
+    if(valor == false){
+        alert("El campo identificación esta incorrecto inténtalo nuevamente");
+        itm = itm + 1;
+        identificacion.focus();
+        return false;
+    }
+    valoHtml = nombre;
+    validarAlfabetico(valoHtml,itm);
 
-function validacion() {
-    var nombre,apellidos,identificacion,edad,profesion,estadosCivil,genero,exprecionNumeros,exprecionTexto;
-    exprecionTexto = /^([a-z A-Z ñáéíóú]{2,60})$/;
-    exprecionNumeros= /[0-9]$/ ;
-  
-    identificacion= document.getElementById("txtIdentificacion").value;
-    nombre= document.getElementById("txtNombre").value;
-    apellidos= document.getElementById("txtApellidos").value;
-    edad= document.getElementById("txtEdad").value;
-    profesion= document.getElementById("txtProfesion").value;
-    genero= document.getElementById("opcionGenero").value;
-    estadosCivil= document.getElementById("opcionEstadoCivil").value;
+    if(valor == false){
+        alert("El campo nombre esta incorrecto inténtalo nuevamente");
+        itm = itm + 1;
+        nombre.focus();
+        return false;
+    }
+    valoHtml = apellidos;
+    validarAlfabetico(valoHtml,itm);
 
-if (identificacion.length > 12){
-    alert("Identificación inválida (Supera la longitud permitida)");
-    return false;
-}
-else if(!exprecionTexto.test(nombre)){
-    alert("El nombre es inválido (Carácter no permitido)");
-    document.forms.nombre.focus();
-    return false;
-}
-else if(!exprecionTexto.test(apellidos)){
-    alert("Los apellido son inválidos (Carácter no permitido)");
-    return false;
-}
-else if(!exprecionTexto.test(profesion)){
-    alert("La profesión es inválida (Carácter no permitido)");
-    return false;
-}
-else if (edad.length > 3){
-    alert("La edad es inválida (Supera la longitud permitida)");
-    return false;
-}
-else{
-    alert ("has sido registrado correctamente "+nombre+" "+apellidos);
-}
+    if(valor == false){
+        alert("El campo apellidos esta incorrecto inténtalo nuevamente");
+        itm = itm + 1;
+        apellidos.focus();
+        return false;
+    }
+    valoHtml = edad ;
+    validarNumeros(valoHtml,itm);
+
+    if(valor == false){
+        alert("El campo edad esta incorrecto inténtalo nuevamente");
+        itm = itm + 1;
+        edad.focus();
+        return false;
+    }
+    valoHtml = profesion;
+    validarAlfabetico(valoHtml,itm);
+    if(valor == false){
+        alert("El campo profesión esta incorrecto inténtalo nuevamente");
+        itm = itm + 1;
+        profesion.focus();
+        return false;
+    }
+    if(itm == 0){
+        alert("Felicidades "+ nombre.value +" "+apellidos.value+ " has sido registado con exitoso");
+    }
 }
